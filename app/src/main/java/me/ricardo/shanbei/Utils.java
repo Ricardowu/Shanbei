4f3795d4a98841154661e52bd6c3584782e0707d
Ricardo <wuhao528@gmail.com>
2017-10-30 17:46:39 +0800

    initial

---
+++ app/src/main/java/me/ricardo/shanbei/Utils.java
@@ -0,0 +1,33 @@
+package me.ricardo.shanbei;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ricardo on 10/30/17.
+ */
+
+public class Utils {
+    public static String getAssetsTxt(Context context, String fileName) {
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            StringBuilder sb = new StringBuilder();
+            String s;
+            while ((s = reader.readLine()) != null) {
+                sb.append(s);
+            }
+            reader.close();
+            inputStreamReader.close();
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
